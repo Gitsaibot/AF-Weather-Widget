07f6e0d6ed6443ceed2c69db6d3f08278c58b4d6
yparitcher <y@paritcher.com>
2023-09-11 00:51:45 -0400

    SunTimeData: use now json sunrise 3.0 api
    met.no shut down version 2.0 based on xml

--- app/src/main/java/net/gitsaibot/af/data/AfMetSunTimeData.java
+++ app/src/main/java/net/gitsaibot/af/data/AfMetSunTimeData.java
@@ -2,10 +2,10 @@ package net.gitsaibot.af.data;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
@@ -23,8 +23,9 @@ import net.gitsaibot.af.util.AfLocationInfo;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
 
 import android.content.ContentResolver;
 import android.content.ContentValues;
@@ -39,7 +40,7 @@ public class AfMetSunTimeData implements AfDataSource {
 	public final static String TAG = "AfMetSunTimeData";
 
 	private final static int NUM_DAYS_MINIMUM = 5;
-	private final static int NUM_DAYS_REQUEST = 15;
+	private final static int NUM_DAYS_REQUEST = 8;
 
 	@SuppressWarnings("serial")
 	private Map<String, Integer> moonPhaseMap = new HashMap<String, Integer>() {{
@@ -75,7 +76,7 @@ public class AfMetSunTimeData implements AfDataSource {
 		mDateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
 		mDateFormat.setTimeZone(mUtcTimeZone);
 		
-		mTimeFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX", Locale.US);
+		mTimeFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mmX", Locale.US);
 	}
 	
 	public static AfMetSunTimeData build(Context context, AfUpdate afUpdate)
@@ -109,155 +110,133 @@ public class AfMetSunTimeData implements AfDataSource {
 		}
 	}
 
-	private List<ContentValues> parseData(
-			InputStream content, long locationId, long currentUtcTime, int maxDays)
-			throws ParseException, XmlPullParserException, IOException
-	{
-		List<ContentValues> contentValuesList = new ArrayList<>();
-		ContentValues contentValues = null;
+	private JSONObject fetchData(String type, Double latitude, Double longitude, Date date, String offset) throws AfDataUpdateException {
+		try {
+			String url = String.format(
+					Locale.US,
+					"https://" + BuildConfig.API_KEY + "/weatherapi/sunrise/3.0/%s?lat=%.1f&lon=%.1f&date=%s&offset=%s",
+					type, latitude, longitude, mDateFormat.format(date), offset);
 
-		float solarnoonElevationValue = 0.0f;
-		Float moonElevationAtStartOfDay = null;
-		
-		XmlPullParser parser = Xml.newPullParser();
-		parser.setInput(content, null);
-		
-		int eventType = parser.getEventType();
-		
-		while (eventType != XmlPullParser.END_DOCUMENT) {
-			switch (eventType) {
-			case XmlPullParser.END_TAG:
-				if (parser.getName().equalsIgnoreCase("time") && contentValues != null)
-				{
-					if (!contentValues.containsKey(AfSunMoonDataColumns.SUN_RISE))
-					{
-						contentValues.put(AfSunMoonDataColumns.SUN_RISE, solarnoonElevationValue >= 0.0f ? 0 : AfSunMoonData.NEVER_RISE);
-					}
+			HttpClient httpClient = AfUtils.setupHttpClient(mContext);
+			HttpGet httpGet = AfUtils.buildGzipHttpGet(url);
+			HttpResponse httpResponse = httpClient.execute(httpGet);
 
-					if (!contentValues.containsKey(AfSunMoonDataColumns.SUN_SET))
-					{
-						contentValues.put(AfSunMoonDataColumns.SUN_SET, AfSunMoonData.NEVER_SET);
-					}
-
-					if (!contentValues.containsKey(AfSunMoonDataColumns.MOON_RISE))
-					{
-						contentValues.put(
-							AfSunMoonDataColumns.MOON_RISE,
-							(moonElevationAtStartOfDay != null && moonElevationAtStartOfDay >= 0.0f) ? 0 : AfSunMoonData.NEVER_RISE);
-					}
-
-					if (!contentValues.containsKey(AfSunMoonDataColumns.MOON_SET))
-					{
-						contentValues.put(AfSunMoonDataColumns.MOON_SET, AfSunMoonData.NEVER_SET);
-					}
-
-					contentValuesList.add(contentValues);
-					contentValues = null;
-					moonElevationAtStartOfDay = null;
-				}
-				break;
-			case XmlPullParser.START_TAG:
-				if (parser.getName().equalsIgnoreCase("time"))
-				{
-					if (contentValuesList.size() < maxDays)
-					{
-						String dateString = parser.getAttributeValue(null, "date");
-						long date = mDateFormat.parse(dateString).getTime();
-
-						contentValues = new ContentValues();
-						contentValues.put(AfSunMoonDataColumns.LOCATION, locationId);
-						contentValues.put(AfSunMoonDataColumns.TIME_ADDED, currentUtcTime);
-						contentValues.put(AfSunMoonDataColumns.DATE, date);
-					}
-				}
-				else if (contentValues != null)
-				{
-					String time = parser.getAttributeValue(null, "time");
-
-					if (time != null)
-					{
-						long timeValue = mTimeFormat.parse(time).getTime();
-
-						if (parser.getName().equalsIgnoreCase("sunrise"))
-						{
-							contentValues.put(AfSunMoonDataColumns.SUN_RISE, timeValue);
-						}
-						else if (parser.getName().equalsIgnoreCase("sunset"))
-						{
-							contentValues.put(AfSunMoonDataColumns.SUN_SET, timeValue);
-						}
-						else if (parser.getName().equalsIgnoreCase("solarnoon"))
-						{
-							solarnoonElevationValue = Float.parseFloat(
-								parser.getAttributeValue(null, "elevation"));
-						}
-						else if (parser.getName().equalsIgnoreCase("moonrise"))
-						{
-							contentValues.put(AfSunMoonDataColumns.MOON_RISE, timeValue);
-						}
-						else if (parser.getName().equalsIgnoreCase("moonset"))
-						{
-							contentValues.put(AfSunMoonDataColumns.MOON_SET, timeValue);
-						}
-						else if (parser.getName().equalsIgnoreCase("moonposition"))
-						{
-							moonElevationAtStartOfDay = Float.parseFloat(
-									parser.getAttributeValue(null, "elevation"));
-
-							float moonphaseValue = Float.parseFloat(
-									parser.getAttributeValue(null, "phase"));
-
-							contentValues.put(
-									AfSunMoonDataColumns.MOON_PHASE,
-									parseMoonPhaseValue(moonphaseValue, parser.getLineNumber()));
-						}
-					}
-				}
-				break;
+			if (httpResponse.getStatusLine().getStatusCode() == 429)
+			{
+				throw new AfDataUpdateException(url, AfDataUpdateException.Reason.RATE_LIMITED);
 			}
-			eventType = parser.next();
+
+			InputStream content = AfUtils.getGzipInputStream(httpResponse);
+			JSONObject jObject = new JSONObject(AfUtils.convertStreamToString(content)).getJSONObject("properties");
+			return jObject;
 		}
-		
-		return contentValuesList;
+		catch (Exception e)
+		{
+			Log.d(TAG, String.format("fetchData(): " + e.getMessage() + " thrown."));
+			throw new AfDataUpdateException();
+		}
+
 	}
 
-	private int parseMoonPhaseValue(float value, int parserLineNumber) throws ParseException
+	private ContentValues parseData(Date date, JSONObject sun, JSONObject moon,
+			long locationId, long currentUtcTime)
+			throws AfDataUpdateException, IOException, JSONException
 	{
-		if (value >= 0.0 && value < 0.5) {
+		try{
+			float solarnoonElevationValue = 0.0f;
+
+			ContentValues contentValues = new ContentValues();
+			contentValues.put(AfSunMoonDataColumns.LOCATION, locationId);
+			contentValues.put(AfSunMoonDataColumns.TIME_ADDED, currentUtcTime);
+Log.d(TAG, String.format("\t%s", date));
+			contentValues.put(AfSunMoonDataColumns.DATE, date.getTime());
+
+			solarnoonElevationValue = Float.parseFloat(sun.getJSONObject("solarnoon").getString("disc_centre_elevation"));
+			JSONObject sunrise = sun.getJSONObject("sunrise");
+			if (!sunrise.isNull("time"))
+			{
+				Log.d(TAG, String.format("Sunrise: %s",mTimeFormat.parse(sunrise.getString("time")).getTime()));
+				contentValues.put(AfSunMoonDataColumns.SUN_RISE, mTimeFormat.parse(sunrise.getString("time")).getTime());
+			}
+			if (!contentValues.containsKey(AfSunMoonDataColumns.SUN_RISE))
+			{
+				contentValues.put(AfSunMoonDataColumns.SUN_RISE, solarnoonElevationValue >= 0.0f ? 0 : AfSunMoonData.NEVER_RISE);
+			}
+
+			JSONObject sunset = sun.getJSONObject("sunset");
+			if (!sunset.isNull("time"))
+			{
+				contentValues.put(AfSunMoonDataColumns.SUN_SET, mTimeFormat.parse(sunset.getString("time")).getTime());
+			}
+			if (!contentValues.containsKey(AfSunMoonDataColumns.SUN_SET))
+			{
+				contentValues.put(AfSunMoonDataColumns.SUN_SET, AfSunMoonData.NEVER_SET);
+			}
+
+			JSONObject moonrise = moon.getJSONObject("moonrise");
+			if (!moonrise.isNull("time"))
+			{
+				contentValues.put(AfSunMoonDataColumns.MOON_RISE, mTimeFormat.parse(moonrise.getString("time")).getTime());
+			}
+			if (!contentValues.containsKey(AfSunMoonDataColumns.MOON_RISE))
+			{
+				contentValues.put(AfSunMoonDataColumns.MOON_RISE, AfSunMoonData.NEVER_RISE);
+			}
+
+			JSONObject moonset = moon.getJSONObject("moonset");
+			if (!moonset.isNull("time"))
+			{
+				contentValues.put(AfSunMoonDataColumns.MOON_SET, mTimeFormat.parse(moonset.getString("time")).getTime());
+			}
+			if (!contentValues.containsKey(AfSunMoonDataColumns.MOON_SET))
+			{
+				contentValues.put(AfSunMoonDataColumns.MOON_SET, AfSunMoonData.NEVER_SET);
+			}
+			contentValues.put(AfSunMoonDataColumns.MOON_PHASE, parseMoonPhaseValue(Float.parseFloat(moon.getString("moonphase"))));
+
+			return contentValues;
+		}
+		catch (Exception e)
+		{
+			Log.d(TAG, String.format("parseData(): " + e.getMessage() + " thrown."));
+			throw new AfDataUpdateException();
+		}
+
+	}
+
+	private int parseMoonPhaseValue(float value)
+	{
+		if (value >= 0.0 && value < 2.0) {
 			return AfSunMoonData.NEW_MOON;
 		}
-		else if (value >= 0.5 && value < 20.0) {
+		else if (value >= 3.0 && value < 72.0) {
 			return AfSunMoonData.WAXING_CRESCENT;
 		}
-		else if (value >= 20.0 && value < 30.0) {
+		else if (value >= 72.0 && value < 108.0) {
 			return AfSunMoonData.FIRST_QUARTER;
 		}
-		else if (value >= 30.0 && value < 49.5) {
+		else if (value >= 388.0 && value < 178.0) {
 			return AfSunMoonData.WAXING_GIBBOUS;
 		}
-		else if (value >= 49.5 && value < 50.5) {
+		else if (value >= 178.0 && value < 182.0) {
 			return AfSunMoonData.FULL_MOON;
 		}
-		else if (value >= 50.5 && value < 70.0) {
+		else if (value >= 182.0 && value < 252.0) {
 			return AfSunMoonData.WANING_GIBBOUS;
 		}
-		else if (value >= 70.0 && value < 80.0) {
+		else if (value >= 252.0 && value < 288.0) {
 			return AfSunMoonData.LAST_QUARTER;
 		}
-		else if (value >= 80.0 && value < 99.5) {
+		else if (value >= 288.0 && value < 358.0) {
 			return AfSunMoonData.WANING_CRESCENT;
 		}
-		else if (value > 99.5 && value <= 100.0) {
+		else if (value >358.0 && value <= 360.0) {
 			return AfSunMoonData.NEW_MOON;
 		}
-		else {
-			throw new ParseException(
-				String.format("parseMoonPhaseValue: value %f out of range", value),
-				parserLineNumber);
-		}
+		return AfSunMoonData.NO_MOON_PHASE_DATA;
 	}
 
-	private void setupDateParameters(long time)
+	private Calendar setupDateParameters(long time)
 	{
 		Calendar calendar = Calendar.getInstance(mUtcTimeZone);
 		
@@ -267,8 +246,10 @@ public class AfMetSunTimeData implements AfDataSource {
 		calendar.add(Calendar.DAY_OF_YEAR, -1);
 		mStartDate = calendar.getTimeInMillis();
 		
-		calendar.add(Calendar.DAY_OF_YEAR, NUM_DAYS_REQUEST - 1);
-		mEndDate = calendar.getTimeInMillis();
+		Calendar endCalendar = (Calendar) calendar.clone();
+		endCalendar.add(Calendar.DAY_OF_YEAR, NUM_DAYS_REQUEST - 1);
+		mEndDate = endCalendar.getTimeInMillis();
+		return calendar;
 	}
 	
 	public void update(AfLocationInfo afLocationInfo, long currentUtcTime)
@@ -286,7 +267,7 @@ public class AfMetSunTimeData implements AfDataSource {
 				throw new AfDataUpdateException("Missing location information. Latitude/Longitude was null");
 			}
 			
-			setupDateParameters(currentUtcTime);
+			Calendar calendar = setupDateParameters(currentUtcTime);
 			
 			int numExistingDataSets = getNumExistingDataSets(afLocationInfo.getId());
 			
@@ -295,26 +276,16 @@ public class AfMetSunTimeData implements AfDataSource {
 			
 			if (numExistingDataSets < NUM_DAYS_MINIMUM)
 			{
-				String url = String.format(
-						Locale.US,
-						"https://" + BuildConfig.API_KEY + "/weatherapi/sunrise/2.0/?lat=%.1f&lon=%.1f&date=%s&offset=%s&days=%d",
-						latitude, longitude,
-						mDateFormat.format(mStartDate),
-						offset, NUM_DAYS_REQUEST);
-
-				HttpClient httpClient = AfUtils.setupHttpClient(mContext);
-				HttpGet httpGet = AfUtils.buildGzipHttpGet(url);
-				HttpResponse httpResponse = httpClient.execute(httpGet);
-
-				if (httpResponse.getStatusLine().getStatusCode() == 429)
-				{
-					throw new AfDataUpdateException(url, AfDataUpdateException.Reason.RATE_LIMITED);
+				List<ContentValues> contentValuesList = new ArrayList<>();
+				for (int i = 1; i < NUM_DAYS_REQUEST; i++) {
+					Date date = calendar.getTime();
+					JSONObject sun = fetchData("sun", latitude, longitude, date, offset);
+					JSONObject moon = fetchData("moon", latitude, longitude, date, offset);
+					ContentValues contentValues = parseData(date, sun, moon,
+						afLocationInfo.getId(), currentUtcTime);
+					contentValuesList.add(contentValues);
+					calendar.add(Calendar.DAY_OF_YEAR, 1);
 				}
-
-				InputStream content = AfUtils.getGzipInputStream(httpResponse);
-
-				List<ContentValues> contentValuesList = parseData(
-						content, afLocationInfo.getId(), currentUtcTime, NUM_DAYS_REQUEST);
 
 				if (contentValuesList.size() > 0)
 				{
