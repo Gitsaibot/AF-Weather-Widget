d976f11f606ca8fc9ba15fb0d1189d5b50247c80
Gitsaibot <Gitsaibot@exmaple.com>
2025-12-08 16:00:43 +0100

    Update gradle and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(
-                getDefaultProguardFile("proguard-android.txt"),
+                getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.txt"
             )
         }
@@ -71,8 +71,8 @@ dependencies {
     implementation("com.google.code.gson:gson:2.13.2")
     implementation("com.google.android.material:material:1.13.0")
     implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.22"))
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.4")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.9.4")
-    implementation("androidx.activity:activity-ktx:1.11.0")
-    implementation("androidx.work:work-runtime:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.10.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.10.0")
+    implementation("androidx.activity:activity-ktx:1.12.1")
+    implementation("androidx.work:work-runtime:2.11.0")
 }
