63b5bfd929a7469ecd67abfff3f5acb7c5916a1d
Gitsaibot <Gitsaibot@exmaple.com>
2026-01-17 01:50:53 +0100

    Bump version to v3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "net.gitsaibot.af"
         minSdk = 29
         targetSdk = 36
-        versionCode = 22
-        versionName = "3.1"
+        versionCode = 23
+        versionName = "3.2"
         buildConfigField("String", "USER_AGENT", "\"" + getKey("user_agent") + "\"")
         buildConfigField("String", "API_KEY", "\"" + getKey("apiKey") + "\"")
         buildConfigField("String", "USER_GEONAMES", "\"" + getKey("user_geonames") + "\"")
